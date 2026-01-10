40c7f1e108e8737f89d7b20273f62d28e2dd1d75
EduardoNOliveira <eduardo@maduinformatica.com.br>
2026-01-10 11:39:21 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const USUARIO_FIXO = "eduardo";
+const USUARIO_FIXO = "Eduardo";
 
 // hash SHA-256 da senha (exemplo: senha = 1960)
 const HASH_SENHA = "c7a5a1e6c93bde91b2c90d4dbf8e1f08c60a8b2d92c8b4e4f5d3d6f8d9c3c2a1";
